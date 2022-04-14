4e867ebdf4f9e9af5d4e45e6aec3dedc4bab3d67
jose <joseantoniolabrador90@gmail.com>
2022-04-14 19:59:30 +0200

    All differentsverses are done

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,10 +9,26 @@ describe("BottleSong", () => {
     expect(result).toEqual(expected);
   })
 
-  it("returns the verse when there is bottles", () => {
+  it("returns the verse when there are two bottles", () => {
     const expected =  "2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.";
     
     const result = bottleVerse(2);
+
+    expect(result).toEqual(expected);
+  })
+
+  it("returns the verse when there is one bottle", () => {
+    const expected =  "1 bottle of beer on the wall, 1 bottle of beer.Take it down and pass it around, no more bottles of beer on the wall.";
+    
+    const result = bottleVerse(1);
+
+    expect(result).toEqual(expected);
+  })
+
+  it("returns the verse when there isn't bottle", () => {
+    const expected =  "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.";
+    
+    const result = bottleVerse(0);
 
     expect(result).toEqual(expected);
   })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 const TWO_BOTTLES = 2
+const ONE_BOTTLE = 1
+const NO_BOTTLE = 0
 
 export function bottleVerse(bottlesNumber: number): any {
+  
+    let bottleVerse = `${bottlesNumber} bottles of beer on the wall, ${bottlesNumber} bottles of beer.Take one down and pass it around, ${bottlesNumber-1} bottles of beer on the wall.`
 
-  let bottleVerse = `${bottlesNumber} bottles of beer on the wall, ${bottlesNumber} bottles of beer.Take one down and pass it around, ${bottlesNumber-1} bottles of beer on the wall.`
+    if (bottlesNumber === TWO_BOTTLES) {
+      return `2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.`
+    }
 
-  if (bottlesNumber === TWO_BOTTLES) {
-    return `2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.`
-  }
+    if (bottlesNumber === ONE_BOTTLE) {
+      return `1 bottle of beer on the wall, 1 bottle of beer.Take it down and pass it around, no more bottles of beer on the wall.`
+    }
+
+    if (bottlesNumber === NO_BOTTLE) {
+      return `No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.`
+    }
 
   return bottleVerse
 
