0f905218e056f71d1067a55ff8e342131499195b
jose <joseantoniolabrador90@gmail.com>
2022-04-14 18:57:44 +0200

    Firsts verses and two bottles verse done.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,18 @@
-import { songVerse } from "./index"
+import { bottleVerse } from "./index"
 
 describe("BottleSong", () => {
-  it("returns the first verses of the song", () => {
+  it("returns the verse of the bottle", () => {
     const expected = "99 bottles of beer on the wall, 99 bottles of beer.Take one down and pass it around, 98 bottles of beer on the wall.";
 
-    const result = songVerse(99);
+    const result = bottleVerse(99);
+
+    expect(result).toEqual(expected);
+  })
+
+  it("returns the verse when there is bottles", () => {
+    const expected =  "2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.";
+    
+    const result = bottleVerse(2);
 
     expect(result).toEqual(expected);
   })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export function songVerse(bottlesNumber: number): any {
-  return `${bottlesNumber} bottles of beer on the wall, ${bottlesNumber} bottles of beer.Take one down and pass it around, ${bottlesNumber-1} bottles of beer on the wall.`
+const TWO_BOTTLES = 2
+
+export function bottleVerse(bottlesNumber: number): any {
+
+  let bottleVerse = `${bottlesNumber} bottles of beer on the wall, ${bottlesNumber} bottles of beer.Take one down and pass it around, ${bottlesNumber-1} bottles of beer on the wall.`
+
+  if (bottlesNumber === TWO_BOTTLES) {
+    return `2 bottles of beer on the wall, 2 bottles of beer.Take one down and pass it around, 1 bottle of beer on the wall.`
+  }
+
+  return bottleVerse
+
 };
