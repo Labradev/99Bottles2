c41f8049f8b75270f42604d2dc64b9c7900b78b5
jose <joseantoniolabrador90@gmail.com>
2022-04-14 17:47:55 +0200

    Init test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,10 @@
-import { wadus } from "./index"
+import { songVerse } from "./index"
 
-describe("Wadus", () => {
-  it("returns 'wadus'", () => {
-    const expected = "wadus";
+describe("BottleSong", () => {
+  it("returns the first verses of the song", () => {
+    const expected = "99 bottles of beer on the wall, 99 bottles of beer.Take one down and pass it around, 98 bottles of beer on the wall.";
 
-    const result = wadus();
+    const result = songVerse(99);
 
     expect(result).toEqual(expected);
   })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export function wadus(): any {
-  return "wadus"
+export function songVerse(bottlesNumber: number): any {
+  return `${bottlesNumber} bottles of beer on the wall, ${bottlesNumber} bottles of beer.Take one down and pass it around, ${bottlesNumber-1} bottles of beer on the wall.`
 };
